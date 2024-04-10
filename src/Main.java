3253631bd8a38f6d642a42add572d5b236f3b7b2
Artem Vasyatkin <dustler98@gmail.com>
2024-04-10 21:50:17 +0300

    training done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("\nTraining");
+        //tack1
+        for ( int a = 0; a < 10; a++) {
+            System.out.println("Итерация цикла " + a);
+        }
+        //task2
+        System.out.println("\n");
+        for ( int i = 2020; i < 2070; i = i +4) {
+            System.out.println("Весокосный год " + i);
+        }
+        //tack3
+        System.out.println("\n");
+        int salary = 65535;
+        int total = 0;
+        for ( int i = 0; i < 12; i++) {
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
     }
 }
